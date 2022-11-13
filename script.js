bd396880c89ac81b57803e9c5076f018114132e7
NadaHesham19 <1806518@eng.asu.edu.eg>
2022-11-13 23:43:56 +0200

    final

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// let navButtons = Array.from(document.querySelectorAll("li a.nav-link"));
-// function buttonUnfocus(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     arr[i].classList.remove("text-primary");
-//   }
-// }
-
 
 // var section = document.querySelectorAll(".section");
 // console.log(section);
@@ -17,63 +10,52 @@
 
 // console.log(sections);
 
-// window.onscroll = function () {
-//   var scrollPosition =
-//     document.documentElement.scrollTop || document.body.scrollTop;
+let acordionicon = Array.from(document.querySelectorAll("i.fa-plus"));
+let acordionbutton = Array.from(
+  document.querySelectorAll("p.fs-5.border-0")
+);
+console.log(acordionicon);
+for (let i = 0; i < acordionbutton.length; i++) {
+  acordionbutton[i].addEventListener("click", convert);
+}
+function convert(e) {
+  let index = acordionbutton.indexOf(e.target);
+  for (let i = 0; i < acordionbutton.length; i++) {
+    if (i == index) {
+      continue;
+    }
+    acordionicon[i].classList.replace("fa-minus", "fa-plus");
+  }
 
-//   for (i in sections) {
-//     if (sections[i] - 90 <= scrollPosition) {
-//       buttonUnfocus(navButtons);
-//       navButtons[i].classList.add("text-primary");
-//     }
-//   }
-// };
+  if (acordionicon[index].classList[1] == "fa-plus") {
+    acordionicon[index].classList.replace("fa-plus", "fa-minus");
+  } else {
+    acordionicon[index].classList.replace("fa-minus", "fa-plus");
+  }
+}
 
-// let acordionicon = Array.from(document.querySelectorAll("i.fa-plus"));
-// let acordionbutton = Array.from(
-//   document.querySelectorAll("p.fs-5.border-0")
-// );
-// console.log(acordionicon);
-// for (let i = 0; i < acordionbutton.length; i++) {
-//   acordionbutton[i].addEventListener("click", convert);
-// }
-// function convert(e) {
-//   let index = acordionbutton.indexOf(e.target);
-//   for (let i = 0; i < acordionbutton.length; i++) {
-//     if (i == index) {
-//       continue;
-//     }
-//     acordionicon[i].classList.replace("fa-minus", "fa-plus");
-//   }
+let FAQsections = Array.from(
+  document.querySelectorAll("div.container div.row div.col-7.p-0")
+);
+let acordionBoxes = Array.from(document.querySelectorAll("div.acordionBoxes"));
+acordionBoxes[0].style.border = "3px solid rgb(2, 117, 216)";
+console.log(acordionBoxes);
+for (let i = 0; i < acordionBoxes.length; i++) {
+  acordionBoxes[i].addEventListener("click", showcontent);
+}
+function showcontent(e) {
+  let index = acordionBoxes.indexOf(e.target);
+  for (let i = 0; i < acordionBoxes.length; i++) {
+    acordionBoxes[i].style.border = "1px solid grey";
+    FAQsections[i].classList.add("d-none");
+  }
+  FAQsections[index].classList.remove("d-none");
 
-//   if (acordionicon[index].classList[1] == "fa-plus") {
-//     acordionicon[index].classList.replace("fa-plus", "fa-minus");
-//   } else {
-//     acordionicon[index].classList.replace("fa-minus", "fa-plus");
-//   }
-// }
+  acordionBoxes[index].style.border = "3px solid rgb(2, 117, 216)";
+}
+console.log(FAQsections);
 
-// let FAQsections = Array.from(
-//   document.querySelectorAll("div.container div.row div.col-7.p-0")
-// );
-// let acordionBoxes = Array.from(document.querySelectorAll("div.acordionBoxes"));
-// acordionBoxes[0].style.border = "3px solid rgb(2, 117, 216)";
-// console.log(acordionBoxes);
-// for (let i = 0; i < acordionBoxes.length; i++) {
-//   acordionBoxes[i].addEventListener("click", showcontent);
-// }
-// function showcontent(e) {
-//   let index = acordionBoxes.indexOf(e.target);
-//   for (let i = 0; i < acordionBoxes.length; i++) {
-//     acordionBoxes[i].style.border = "1px solid grey";
-//     FAQsections[i].classList.add("d-none");
-//   }
-//   FAQsections[index].classList.remove("d-none");
-
-//   acordionBoxes[index].style.border = "3px solid rgb(2, 117, 216)";
-// }
-// console.log(FAQsections);
-const accordion = document.getElementsByClassName('container');
+const accordion = document.getElementsByClassName('acc-container');
 
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
